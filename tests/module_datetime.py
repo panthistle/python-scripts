4a2718142dc1bbcf33c0b93f06ac2a1b0e739738
Pan Thistle <60625336+panthistle@users.noreply.github.com>
2022-06-01 14:00:02 +0300

    Update module_datetime.py

--- tests/module_datetime.py
+++ tests/module_datetime.py
@@ -1,5 +1,5 @@
 import datetime
-# import pytz
+import pytz
 
 # datetime commands
 #print(dir(datetime))
@@ -12,7 +12,10 @@ import datetime
 # date commands
 #print(dir(datetime.date))
 
-d = datetime.date(2000, 1, 1)
+#help(datetime.date)
+# datetime.date(year, month, day) --> date object
+
+d = datetime.date(2000, 1, 25)
 bday = datetime.date(2022, 10, 14)
 
 today = datetime.date.today()
@@ -24,12 +27,11 @@ dayid = today.weekday()     # Monday is 0 and Sunday is 6
 dayid = today.isoweekday()  # Monday is 1 and Sunday is 7
 #print(dayid)
 
-def days_to_bday(bday):
-    today = datetime.date.today()
-    to_bday = bday - today
-    return to_bday.days
+def days_between(fromdate, todate):
+    td = todate - fromdate
+    return td.days
 
-dtb = days_to_bday(bday)
+dtb = days_to_bday(today, bday)
 #print(dtb)
 
 
@@ -37,14 +39,21 @@ dtb = days_to_bday(bday)
 #
 # ------------------------------ timedelta -------------------------------------
 
+# timedelta commands
+#print(dir(datetime.timedelta))
+
+#help(datetime.timedelta)
 # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, 
-#                   minutes=0, hours=0, weeks=0)
+#                   minutes=0, hours=0, weeks=0) --> timedelta object
 
-hrs = datetime.timedelta(hours=12)
-#print(today + hrs)
-
-# d2 = d + hrs
-# timedelta = d + d2
+# timedelta is the difference between two datetime values
+# if we add/subtract timedelta to/from date, we get another date
+tdelta = datetime.timedelta(days=7)
+newdate = today + tdelta
+#print(newdate)
+# if we add/subtract date to/from date, we get timedelta
+tdelta = newdate - today
+#print(tdelta)
 
 
 # ------------------------------------------------------------------------------
@@ -54,9 +63,14 @@ hrs = datetime.timedelta(hours=12)
 # time commands
 #print(dir(datetime.time))
 
-# function to set the time
-t = datetime.time(9, 30, 45, 100000)
+#help(datetime.time)
+# datetime.time(hour=12, minute=0, second=0, microsecond=0, 
+#               tzinfo=None) --> time object
+
+# example
+t = datetime.time(3, 12, 45, 100000)
 #print(t)
+#print(t.hour)
 
 
 # ------------------------------------------------------------------------------
@@ -66,40 +80,50 @@ t = datetime.time(9, 30, 45, 100000)
 # datetime commands
 #print(dir(datetime.datetime))
 
-dt = datetime.datetime.today()
-dtnow = datetime.datetime.now()
-print(dt)
-print(dtnow)
+help(datetime.datetime)
+# datetime.datetime(year, month, day, hour=0, minute=0, second=0, 
+#                   microsecond=0, tzinfo=None) --> datetime object
 
-# dt = datetime.datetime(2016, 7, 24, 12, 30, 45, tzinfo=pytz.UTC)
-# print(dir(dt))
+# not timezone aware
+dt_today = datetime.datetime.today()
+dt_now = datetime.datetime.now()
+dt_utcnow = datetime.datetime.utcnow()
+#print(dt_today)
+#print(dt_now)
+#print(dt_utcnow)
 
-# dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
+# using pytz module
+dt = datetime.datetime(2000, 7, 24, 12, 30, 45, tzinfo=pytz.UTC)
+# print(dt)
+
+dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
 # print(dt_utcnow)
 
-# dt_utcnow2 = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+dt_utcnow2 = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
 # print(dt_utcnow2)
 
-# dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
-# print(dt_mtn)
+dt_ath = dt_utcnow.astimezone(pytz.timezone('Europe/Athens'))
+# print(dt_ath)
 
-dt_mtn = datetime.datetime.now()
+# timezones info
+# for tz in pytz.all_timezones:
+    # print(tz)
 
-# mtn_tz = pytz.timezone('US/Mountain')
-# dt_mtn = mtn_tz.localize(dt_mtn)
+# convert from naive to timezone aware
+dt_athens = datetime.datetime.now()
+athens_tz = pytz.timezone('Europe/Athens')
+dt_athens = athens_tz.localize(dt_athens)
+# print(dt_athens)
 
-print(dt_mtn)
-
-# dt_east = dt_mtn.astimezone(pytz.timezone('US/Eastern'))
-# print(dt_east)
+# convert between different timezones
+dt_paris = dt_athens.astimezone(pytz.timezone('Europe/Paris'))
+# print(dt_paris)
 
 # strftime - Datetime to String
-dt_str = dt_mtn.strftime('%B %d, %Y')
+dt_str = dt_paris.strftime('%B %d, %Y')
 #print(dt_str)     
 
 # strptime - String to Datetime
-dt_str = 'July 24, 2016'
+dt_str = 'May 31, 2022'
 dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
 #print(dt)
-
-
