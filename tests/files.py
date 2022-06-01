fd0bc41923eb8b8bf48f85e7097ad8fa0b660044
Pan Thistle <60625336+panthistle@users.noreply.github.com>
2022-06-01 14:10:07 +0300

    Update files.py

--- tests/files.py
+++ tests/files.py
@@ -14,7 +14,7 @@
 
 # ------------------------------------------------------------------------------
 #
-# ------------------------- reading files --------------------------------------
+# ----------------------- reading text files -----------------------------------
 
 with open("files_test_read.txt", "r") as f:
 
@@ -75,7 +75,7 @@ print(f.closed)
 
 # ------------------------------------------------------------------------------
 #
-# ------------------------- writing files --------------------------------------
+# ----------------------- writing text files -----------------------------------
 
 with open("files_test_write.txt", "w") as f:
 
@@ -110,3 +110,19 @@ with open("files_test_write.txt", "w") as f:
 #             wf.write(rf_chunk)
 #             rf_chunk = rf.read(chunk_size)
 
+
+# ------------------------------------------------------------------------------
+#
+# ------------------------- parsing csv files ----------------------------------
+
+import csv
+
+with open('files_csv_parse_old.csv', 'r') as fr:
+    reader = csv.DictReader(fr)
+    with open('files_csv_parse_new.csv', 'w') as fw:
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(fw, fieldnames=fieldnames, delimiter='\t')
+        writer.writeheader()
+        for line in reader:
+            del line['email']
+            writer.writerow(line)
